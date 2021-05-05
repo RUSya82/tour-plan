c09b576e12028ba648bb6d1107d3890c02f76886
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-05 22:44:22 +0500

    create filesystem for styles

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+const concat = require("gulp-concat");
+const autoprefix = require("gulp-autoprefixer");
+
+// source directories
+const sourceSass = "./sass/main.sass";
+const distCSS = "./css/";
+
+gulp.task('sass', () => {
+    return gulp.src(sourceSass)
+        .pipe(sass({
+            // outputStyle: 'compressed'
+        }))
+        .pipe(concat('main.css'))
+        .pipe(autoprefix({
+            browsersList: ['last 8 versions'],
+            cascade: true
+        }))
+        .pipe(gulp.dest(distCSS))
+});
+
+gulp.task('serve', function(done) {
+
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch("sass/*.s*ss", gulp.series('sass'));
+    gulp.watch("./*.*").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+    done();
+});
+
+gulp.task('sass', gulp.series('sass'));
+gulp.task('default', gulp.series('sass', 'serve'));
