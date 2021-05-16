72fb0ddfa3630069ec876e8062f1c6f7e92b0ef1
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-16 20:09:52 +0500

    create yandex map with API

--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ const hotelSwiper = new Swiper('.swiper-container', {
 document.addEventListener('click', e => {
     console.log(e.target)
 })
+
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("hotel-map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [12.920828, 100.859919],
+        //12.920828, 100.859919
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 15
+    });
+    var myPlacemark = new ymaps.Placemark([12.920828, 100.859919], {
+        balloonContent: '<p> я тут был, ещё хочу</p>'
+    });
+    myMap.geoObjects.add(myPlacemark);
+}
