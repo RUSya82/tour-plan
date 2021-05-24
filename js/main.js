9d89eec459335165a9f8242fa04c8ddff59485c1
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-25 02:27:59 +0500

    create send mail for forms

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const hotelSwiper = new Swiper('.hotel-slider__container', {
     // Optional parameters
     loop: true,
 
-
     // Navigation arrows
     navigation: {
         nextEl: '.hotel-slider__arrow--next',
@@ -14,9 +13,9 @@ const hotelSwiper = new Swiper('.hotel-slider__container', {
     },
 });
 
-document.addEventListener('click', e => {
-    console.log(e.target)
-})
+// document.addEventListener('click', e => {
+//     console.log(e.target)
+// })
 const reviewsSwiper = new Swiper('.reviews-slider__container', {
     // Optional parameters
     loop: true,
@@ -45,3 +44,39 @@ const menuToggle = () => {
 };
 
 menuToggle();
+
+const postData = (body) => {
+    return fetch('send.php', {
+        body: body,
+        method: 'POST',
+    });
+};
+const bindForm = (form) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = e.target.dataset.name;
+        const formData = new FormData(e.target);
+        const data = {
+            'formName': name,
+        };
+        formData.forEach((item, index) => data[index] = item);
+        postData(JSON.stringify(data))
+            .then((response) => {
+                if (response.status !== 200) {
+                    throw new Error('status not 200');
+                }
+                return response.text()
+            }).then((response) => {
+            form.reset();
+        }).catch((error) => {
+            console.error(error);
+        });
+    })
+};
+const formListener = () => {
+    const feedBackForm = document.querySelector('.feedback-form');
+    const subscribeForm = document.querySelector('.subscribe-form');
+    bindForm(feedBackForm);
+    bindForm(subscribeForm);
+};
+formListener();
