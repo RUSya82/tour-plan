2d844b82b85c27b499bb89ecaf6b9fd0b2920b56
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-27 05:11:32 +0500

    control commit lesson 28

--- js/main.js
+++ js/main.js
@@ -1,69 +1,72 @@
 import { Modal} from "./modal.js";
 import { bindForm} from "./bindForm.js";
 
-const modal = new Modal({});
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = new Modal({});
 
-const hotelSwiper = new Swiper('.hotel-slider__container', {
-    // Optional parameters
-    loop: true,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.hotel-slider__arrow--next',
-        prevEl: '.hotel-slider__arrow--prev',
-    },
-    keyboard: {
-        enable: true,
-        onlyInViewport: true
-    },
-});
+    const hotelSwiper = new Swiper('.hotel-slider__container', {
+        // Optional parameters
+        loop: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.hotel-slider__arrow--next',
+            prevEl: '.hotel-slider__arrow--prev',
+        },
+        keyboard: {
+            enable: true,
+            onlyInViewport: true
+        },
+    });
 
 // document.addEventListener('click', e => {
 //     console.log(e.target)
 // })
-const reviewsSwiper = new Swiper('.reviews-slider__container', {
-    // Optional parameters
-    loop: true,
+    const reviewsSwiper = new Swiper('.reviews-slider__container', {
+        // Optional parameters
+        loop: true,
 
-    centeredSlides: true,
-    slidesPerView: 1,
-    speed: 500,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.reviews-slider__button--next',
-        prevEl: '.reviews-slider__button--prev',
-    },
-    keyboard: {
-        enable: true,
-        onlyInViewport: true
-    },
+        centeredSlides: true,
+        slidesPerView: 1,
+        speed: 500,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.reviews-slider__button--next',
+            prevEl: '.reviews-slider__button--prev',
+        },
+        keyboard: {
+            enable: true,
+            onlyInViewport: true
+        },
+    });
+
+    const menuToggle = () => {
+        const menu = document.querySelector('.navbar-menu');
+        const menuButton = document.querySelector('.menu-button');
+        menuButton.addEventListener('click', (e) => {
+            menu.classList.toggle('navbar-menu--visible');
+        });
+    };
+    menuToggle();
+
+    const formListener = () => {
+        const feedBackForm = document.querySelector('.feedback-form');
+        const subscribeForm = document.querySelector('.subscribe-form');
+        const modalForm = document.querySelector('.modal-form');
+        if (feedBackForm){
+            bindForm(feedBackForm, () => {
+                window.location.assign('thanks.html');
+            });
+        }
+        if(subscribeForm){
+            bindForm(subscribeForm);
+        }
+        if(modalForm){
+            bindForm(modalForm, () => {
+                modal.closeModal();
+            });
+        }
+
+    };
+    formListener();
 });
 
-const menuToggle = () => {
-    const menu = document.querySelector('.navbar-menu');
-    const menuButton = document.querySelector('.menu-button');
-    menuButton.addEventListener('click', (e) => {
-        menu.classList.toggle('navbar-menu--visible');
-    });
-};
-menuToggle();
-
-const formListener = () => {
-    const feedBackForm = document.querySelector('.feedback-form');
-    const subscribeForm = document.querySelector('.subscribe-form');
-    const modalForm = document.querySelector('.modal-form');
-    if (feedBackForm){
-        bindForm(feedBackForm, () => {
-            window.location.assign('thanks.html');
-        });
-    }
-    if(subscribeForm){
-        bindForm(subscribeForm);
-    }
-    if(modalForm){
-        bindForm(modalForm, () => {
-            modal.closeModal();
-        });
-    }
-
-};
-formListener();
